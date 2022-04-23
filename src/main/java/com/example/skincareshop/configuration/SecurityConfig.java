5681ec22eb8d1650a59a1a30402089b2fe347635
Alina Mocanu <alinamocanu2@gmail.com>
2022-04-23 20:58:44 +0300

    add jpa auth

---
+++ src/main/java/com/example/skincareshop/configuration/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.skincareshop.configuration;public class SecurityConfig {
+}
---
+++ src/main/java/com/example/skincareshop/domain/security/User.java
@@ -0,0 +1,41 @@
+package com.example.skincareshop.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String username;
+    private String password;
+
+    @Singular
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name="authority_id", referencedColumnName = "id"))
+    private Set<Authority> authorities;
+
+
+    @Builder.Default
+    private Boolean enabled = true;
+
+    @Builder.Default
+    private Boolean accountNotExpired = true;
+
+    @Builder.Default
+    private Boolean accountNotLocked = true;
+
+    @Builder.Default
+    private Boolean credentialsNotExpired = true;
+
+}
